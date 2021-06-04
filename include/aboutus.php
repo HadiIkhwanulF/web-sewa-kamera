ad3fae3499f42b09bbd46caff1255bd69afdbd1b
HadiIkhwanulF <85212076+HadiIkhwanulF@users.noreply.github.com>
2021-06-04 21:44:44 +0700

    About Us
    Frontend About Us

---
+++ include/aboutus.php
@@ -0,0 +1,62 @@
+
+    <section id="blog-header">
+      <div class="container">
+        <h1 class="text-white">ABOUT US</h1>
+      </div>
+    </section><br><br>
+    <section id="blog-list">
+      <main role="main" class="container">
+        <div class="row">
+          <div class="col-md-9 blog-main">
+            <div class="blog-post">
+              <h2 class="blog-post-title">Kamera Kita</h2>
+              <p class="blog-post-meta">Jun 4, 2021 by <a href="#">Hadi</a></p>
+
+              <p>Merupakan jenis usaha baru di lingkup Malang dan sekitarnya.
+                Usaha persewaan kamera & Lensa ini merupakan usaha yang murah dan strategis.
+                Usaha ini akan membantu para fotografer/Konsumen yang ingin melakukan pemotretan suatu obyek namun 
+                terhalang tidak mempunyai biaya untuk membeli <b>Kamera/Lensa</B> dan bagi suatu organisasi yang membutuhkan 
+                dokumentasi untuk event-eventnya bisa menyewa di usaha kami.</p>
+              <h2>Kelebihan</h2>
+              <p> 1. Better Price : Biaya Terjangkau, Variatif harga sewa sesuai kantong budget, banyak diskon,ada voucher dan membercard <br>
+                  2. Better Product : Kualitas Kamera dan Asesories terjaga dengan baik, siap pakai untuk mengabadikan moment anda <br>
+                  3. Better People : Team Kami memberi pelayanan pemindahan data gratis di Office, Live tutorial untuk pemula dgn singkat dan padat, Pelayanan 5 S <br>
+                  4. Better Place : Tempat Kami nyaman, dekat dengan berbagai kampus <br>
+                  5. Better Service : Kami melayani jasa antar ambil, lebih fleksible dan cepat.</p> 
+              <h3>Visi & Misi</h3><br>
+              <h4>Visi</h4> 
+              <p>Menjadi perusahaan persewaan Kamera & Lensa terkemuka, terlengkap, dan terpercaya melalui pelayanan cepat dan prima.</p>
+              <h4>Misi</h4>
+              <P>
+              - Menyediakan alat-alat photography dan videography yang terbaru, berkualitas, dan beragam. <br>
+
+              - Pelayanan tulus dan ramah pada setiap pelanggan. <br>
+
+              - Mampu memberikan solusi bagi setiap konsumen Pondok Lensa. <br>
+
+              - Menjadi tempat bekerja yang nyaman bagi seluruh karyawan. <br>
+              </P>
+              
+            </div><br><br><!-- /.blog-post -->
+          </div><!-- /.blog-main -->
+      
+          <aside class="col-md-3 blog-sidebar">
+      
+            <div class="p-4">
+              <h4 class="font-italic">Social Media</h4>
+              <ol class="list-unstyled">
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instagram</a></li>
+              </ol>
+            </div>
+          </aside><!-- /.blog-sidebar -->
+      
+        </div><!-- /.row -->
+      
+      </main><!-- /.container -->
+    </section><br><br>
+    
+  </body>
+</html>
